95a1dd4a338bca4f41dec70eee3299637c39146e
nico-devops <nico-devops@gmail.com>
2024-07-12 21:43:25 +0200

    creation script install_nfs.sh install_wordpress.sh

---
+++ clusterk8s/install_nfs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# install nfs server
+# Variables
+
+IP_RANGE=$(dig +short srvhaprox | sed s/".[0-9]*$"/.0/g)
+
+# Functions
+
+prepare_directories(){
+    sudo mkdir -p /srv/wordpress/{db,files}
+    sudo chmod 777 -R /srv/wordpress/
+}
+
+install_nfs(){
+    sudo apt-get install -y nfs-kernel-server 2>&1 > /dev/null
+}
+
+set_nfs(){
+    sudo echo "/srv/wordpress/db ${IP_RANGE}/24(rw,sync,no_root_squash,no_subtree_check)">/etc/exports
+    sudo echo "/srv/wordpress/files ${IP_RANGE}/24(rw,sync,no_root_squash,no_subtree_check)">>/etc/exports
+}
+
+run_nfs(){
+    sudo systemctl restart nfs-server rpcbind
+    sudo exportfs -a
+}
+
+# Script
+
+prepare_directories
+install_nfs
+set_nfs
+run_nfs
